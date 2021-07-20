0fa6c41d26fd64bbc157ff761c6111879b0f7ed4
morecode <761170536@qq.com>
2021-07-20 18:17:36 +0800

    rebuild

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Header from './Header'
-import ChangeLog from './Markdown'
 
 ReactDOM.render(
   <React.StrictMode>
     <Header></Header>
-    <App />
-    <ChangeLog></ChangeLog>
   </React.StrictMode>,
   document.getElementById('root')
 );
