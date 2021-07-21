d1527f3c214720e64ed176631ab58eba20ec83d3
morecode <761170536@qq.com>
2021-07-21 09:19:43 +0800

    master分支内容更改

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css'
 import './index.css';
-import Header from './components/Header'
+import Structure from './components/Structure'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header></Header>
+   <Structure/>
   </React.StrictMode>,
   document.getElementById('root')
 );
